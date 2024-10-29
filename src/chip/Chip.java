573b03a9d1ed32df880a9a8e3f76e171bb84e605
gausszhou <gausszhou@qq.com>
2024-10-30 00:51:50 +0800

    feat: Load program and Decode opcode

--- src/chip/Chip.java
+++ src/chip/Chip.java
@@ -1,5 +1,10 @@
 package chip;
 
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class Chip {
 
 	// 内存
@@ -22,6 +27,8 @@ public class Chip {
 	// 显示输出
 	private byte[] display;
 
+	private boolean needRedraw;
+
 	public void init() {
 		memory = new char[4096];
 		V = new char[16];
@@ -35,7 +42,7 @@ public class Chip {
 
 		keys = new byte[16];
 		display = new byte[64 * 32];
-		// display[1] = 1;
+
 	}
 
 	public void run() {
@@ -45,21 +52,83 @@ public class Chip {
 		System.out.println(Integer.toHexString(opcode) + ": ");
 
 		switch (opcode & 0xF000) { // get head 1 type
+			// 1NNN 跳转到地址 NNN
+			case 0x1000:
+				pc = (char) (opcode & 0x0FFF);
+				break;
+			// 2NNN 在 NNN 处调用子例程。
+			case 0x2000:
+				stack[stackPointer] = pc;
+				stackPointer += 1;
+				pc = (char) (opcode & 0x0FFF);
+				break;
+			// 3XNN 如果 VX 等于 NN，则跳过下一条指令
+			case 0x3000:
+				int x3 = (opcode & 0x0F00) >> 8;
+				// TODO
+				break;
+			// 4XNN 如果 VX 不等于 NN，则跳过下一条指令
+			case 0x4000:
+				int x4 = (opcode & 0x0F00) >> 8;
+				// TODO
+				break;
+			// 5XY0 如果 VX 等于 VY，则跳过下一条指令
+			case 0x5000:
+				int x5 = (opcode & 0x0F00) >> 8;
+				int y5 = (opcode & 0x00F0) >> 4;
+				// TODO
+				break;
+			// 6XNN 将 VX 设置为 NN
+			case 0x6000:
+				int x6 = (opcode & 0x0F00) >> 8;
+				int nn6 = (opcode & 0x00FF);
+				V[x6] = (char) nn6;
+				pc += 2;
+				break;
+			// 7XNN 将 NN 添加到 VX
+			case 0x7000:
+				int x7 = (opcode & 0x0F00) >> 8;
+				int nn7 = (opcode & 0x00FF);
+				V[x7] = (char) ((V[x7] + nn7) & 0xFF);
+				pc += 2;
+				break;
+			// 8XNN
 			case 0x8000:
 				switch (opcode & 0x000F) {
-					case 0x0000: // set VX value to VY
+					// 8XY0 将 VX 设置为 VY 的值
+					case 0x0000:
 
 						break;
+					// 8XY1 将 VX 设置为 VX 位或 VY Vx |= Vy
+					// 8XY2 将 VX 设置为 VX 位与 VY Vx &= Vy
+					// 8XY3 将 VX 设置为 VX 位异或 VY Vx ^= Vy
+					// 8XY4 将 VY 添加到 VX 中
 
 					default:
-						System.err.println("Unsupported Opcode!");
+						System.err.println("Unsupported Opcode! 0x8000");
 						System.exit(0);
 						break;
 				}
 
 				break;
+			// ANNN 将 I 设置为地址 NNN => I = NNN
+			case 0xA000:
+				I = (char) (opcode & 0x0FFF);
+				pc += 2;
+				break;
+			// BNNN 跳转到地址 NNN 加 V0 => PC = V0 + NNN
+			case 0xB000:
+				stack[stackPointer] = pc;
+				stackPointer += 1;
+				pc = (char) (V[0] + (opcode & 0x00FFF));
+
+				break;
+			case 0xD000:
+				// TODO
+				pc += 2;
+				break;
 			default:
-				System.err.println("Unsupported Opcode!");
+				System.err.println("Unsupported Opcode!" + opcode);
 				break;
 		}
 		// execute opcode
@@ -68,4 +137,29 @@ public class Chip {
 	public byte[] getDisplay() {
 		return display;
 	}
+
+	public boolean isNeedRedraw() {
+		return needRedraw;
+	}
+
+	public void removeDrawFlag() {
+		needRedraw = false;
+	}
+
+	public void loadProgram(String file) {
+
+		try (DataInputStream input = new DataInputStream(new FileInputStream(new File(file)));) {
+			int offset = 0;
+			while (input.available() > 0) {
+				//
+				memory[0x200 + offset] = (char) (input.readByte() & 0xFF);
+				offset++;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+
+	}
+
 }
--- src/emu/Main.java
+++ src/emu/Main.java
@@ -2,11 +2,36 @@ package emu;
 
 import chip.Chip;
 
-public class Main {
+public class Main extends Thread {
+
+    private Chip chip8;
+    private ChipFrame frame;
+
+    public Main(){
+        chip8 = new Chip();
+        chip8.init();
+        chip8.loadProgram("./programs/pong2.c8");
+        frame = new ChipFrame(chip8);
+    }
+
+    public void run() {
+        while (true) {
+            chip8.run();
+            if (chip8.isNeedRedraw()) {
+                frame.repaint();
+                chip8.removeDrawFlag();
+            }
+            try {
+                Thread.sleep(16);
+            } catch (Exception e) {
+                System.err.println(e);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        Chip c = new Chip();
-        c.init();
-        // c.run();
-        ChipFrame frame = new ChipFrame(c);
+        Main main = new Main();
+        main.start();
+
     }
 }
